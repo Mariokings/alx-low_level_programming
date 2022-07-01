754f8659b88d0d3300fc03fa3579e4f6f3ba477e
Mariokings <ozorkingsley168@yahoo.com>
2022-07-01 11:01:52 -0700

    6-size.c checking

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,14 @@ int main(void)
 {
 	char i;
 	int h;
-	double u;
+	long int u;
+	long long int g;
+	float q;
+
 	printf("The size of a char is: %lu\n", (unsigned long)sizeof(i));
 	printf("The size of an int is: %lu\n", (unsigned long)sizeof(h));
-	printf("The size of a double is: %lu\n", (unsigned long)sizeof(u));
+	printf("The size of a long int is: %lu\n", (unsigned long)sizeof(u));
+	printf("the size of a long long int is: %lu\n", (unsigned long)sizeof(g))
+	printf("The size of a float is: %lu\n", (unsigned long)sizeof(q))
 	return (0);
 }
