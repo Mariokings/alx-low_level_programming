d5064abf7a4eef3b0253c9f63fb0777c00d2a437
Mariokings <ozorkingsley168@yahoo.com>
2022-07-04 21:27:18 -0700

    testing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
 /**
- * main - No entry point
- *
- * Return: Always 0 (Success!)
+ * main - main block
+ * Description - Print the alphabets
+ * Return: 0
  */
-
 int main(void)
 {
-	int i, j, k;
+	char c = 'a';
 
-	for (i = 0; i <= 7; i++)
+	while (c <= 'z')
 	{
-		for (j = 1; j <= 8; j++)
+		if (c != 'e' && c != 'q')
 		{
-			if (i < j)
-			{
-				for (k = 2; k <= 9; k++)
-				{
-					if (j < k)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(k + '0');
-					if (i != 7 || j != 8 || k != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-				}
-			}
+			putchar(c);
 		}
+		c++;
 	}
 	putchar('\n');
 	return (0);
-}
