6be57918ec00ecab2131aa8b21aeb22522ce23e1
Mariokings <ozorkingsley168@yahoo.com>
2022-07-06 09:36:56 -0700

    checking

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check description
  *
  * Description: It prints the word Holberton, followed by a new line.
  *
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char cool[8] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(cool[i]);
+		_putchar(word[i]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
