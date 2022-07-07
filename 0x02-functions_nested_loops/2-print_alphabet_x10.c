d31d99b6567b8036120d7ad49f2faf89f11ded5c
Mariokings <ozorkingsley168@yahoo.com>
2022-07-06 23:11:26 -0700

    checking

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,13 @@
-#include "main.h"
-
-/**
- * print_alphabet_x10 - No entry point
- *
- * Return: On success Nothing.
+#include "main.h"                                                                                   /**                                                * print_alphabet_x10 - No entry point             *                                                 * Return: On success Nothing.
  */
 
-void print_alphabet_x10(void)
-{
-	char alpha;
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
-		{
-			_putchar(alpha);
-		}
-		_putchar('\n');
-	}
+void print_alphabet_x10(void)                     {                                                         char alpha;
+        int i;
+                                                          for (i = 0; i <= 9; i++)
+        {
+                for (alpha = 'a'; alpha <= 'z'; alpha++)
+                {
+                        _putchar(alpha);
+                }
+                _putchar('\n');                           }
 }
