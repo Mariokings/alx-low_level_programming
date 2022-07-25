46323f218f9a9e4091f5f2f5b48c91b43482ef3c
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 22:40:11 -0700

    check

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc >= 3)
+	if (argc == 3)
 	{
 		int sum;
 		int i;
 
 		sum = 0;
-		for (i = 1; i > 3; i++)
+		for (i = 1; i > argc; i++)
 		{
 			sum = sum * argv[i];
 		}
