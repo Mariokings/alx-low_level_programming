11473e1e4f4e1bcb4e4cd6e4004f7bb3ea51e685
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 23:37:41 -0700

    checking

---
+++ 0x0A-argc_argv/3-mult.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: number of arguments passed
+ *
+ * @argv: one dimmensional array of strings
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int sum;
+		int i;
+
+		sum = 1;
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum * atoi(argv[i]);
+		}
+		printf("The sum is: %d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
