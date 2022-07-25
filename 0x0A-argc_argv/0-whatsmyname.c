c3422705f94d0b8335ad99611179957e110fee2c
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 20:30:39 -0700

    check

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the no. of arg and also the strings
@@ -9,8 +9,7 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0);
 }
