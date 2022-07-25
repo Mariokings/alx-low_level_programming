22f210c6e7ac72cae5587c63041884c7cb40414b
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 20:20:30 -0700

    checking

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the no. of arg and also the strings
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
