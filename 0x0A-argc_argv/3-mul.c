3b865104914b500a7061e6a402ac6c580816e3c8
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 23:57:41 -0700

    checking

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,30 +3,23 @@
 
 /**
  * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * @argc: number of arguments passed
- *
- * @argv: one dimmensional array of strings
- *
- * Return: Always 0.
+ * Return: 0 if no errors, else 1
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	int sum;
-	int i;
+int a, b, c;
 
-	sum = 1;
-	for (i = 1; i < argc; i++)
-	{
-		sum = sum * atoi(argv[i]);
-	}
-	printf("The sum is: %d\n", sum);
-
-	return (0);
+if (argc != 3)
+{
+puts("Error");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+c = a *b;
+printf("%d\n", c);
+return (0);
 }
