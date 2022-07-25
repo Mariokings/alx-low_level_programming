5d24418b7e476b82e93e62ff9cce6b6e1080df55
Mariokings <ozorkingsley168@yahoo.com>
2022-07-24 20:43:08 -0700

    check

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  * Return: Always 0.
  */
 
-void main(int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
