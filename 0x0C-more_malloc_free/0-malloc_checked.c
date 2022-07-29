5a8265fc5ef71c258cfa3f6a8757d9d15dd142ff
Mariokings <ozorkingsley168@yahoo.com>
2022-07-28 19:55:06 -0700

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+ (14 sloc)  232 Bytes
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of buffer
+ * Return: pointer to buffer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
