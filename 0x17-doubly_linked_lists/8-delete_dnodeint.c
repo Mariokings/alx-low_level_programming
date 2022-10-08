1b2bede4ceb93f23914a011df745015bebcc3c24
Mariokings <ozorkingsley168@yahoo.com>
2022-10-07 23:31:35 -0700

    check

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,6 @@ int delete_within(dlistint_t **head, unsigned int index)
 	dlistint_t *temp, *tail;
 	size_t count = 1;
 
-	temp = *head;
 	if (temp == NULL)
 		return (-1);
 	while (count <= index)
